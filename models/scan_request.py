1db69cf877c8e7b87d2d8bf68c7d4e7e6a1070cf
baonq-me <quocbao747@gmail.com>
2024-12-30 11:47:46 +0700

    Default timeout to 15 seconds

--- models/scan_request.py
+++ models/scan_request.py
@@ -6,7 +6,7 @@ class DomainRequest(BaseModel):
 
 
 class DomainRequestParams(BaseModel):
-    timeout: int = Field(description="Timeout in seconds", default="3", example="3")
+    timeout: int = Field(description="Timeout in seconds", default="15", example="15")
     bypass_cache: int = Field(description="Set to 1 to bypass cache", default="0", example="0")
     uri_scheme: str = Field(description="URL schemes: http or https", default="https", example="https")
     pageload_wait_seconds: float = Field(description="Time to wait for page load", default="5.0", example="5.0")
